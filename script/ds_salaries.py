d0a19b48a12c9aef167173fd177bfe05f746783f
Philip Penchev <132268820+filav1000@users.noreply.github.com>
2023-06-02 09:10:26 +0300

    Update ds_salaries.py

--- script/ds_salaries.py
+++ script/ds_salaries.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import geopandas as gpd
 
+# load the dataset
+df = pd.read_csv('/home/user/Desktop/ds_salaries.csv')
+print(df.head())
